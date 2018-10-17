bebaafe122b101e40bbc13c66a27db18cdca0f7b
kostya bahm <kostyabah2611@gmail.com>
2018-10-17 15:20:16 +0300

    ready_webpack

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -1,1 +1,71 @@
-module.exports = {};
+var path = require("path");
+//console.log(__dirname)
+module.exports = {
+    entry: "./src/frontend/root.js",
+    output: {
+        path: __dirname + '/dist',
+        publicPath: "/dist",
+        filename: "build.js"
+    },
+    mode : 'development',
+    devServer: {
+        contentBase : path.join(__dirname, '/src'),
+        port : 3000,
+        //inline: true,
+        //hot: true
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use : {
+                    loader: "babel-loader",
+                    options : {
+                        plugins: [
+                            ["transform-react-jsx"],
+                            ["transform-class-properties"]
+                        ]
+                    }
+                    
+                },
+                
+                exclude: [/node_modules/, /public/],   
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!autoprefixer-loader!less-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.gif$/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: /\.svg/,
+                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            },
+            {
+                test: /\.txt$/,
+                use: 'raw-loader'
+            }
+        ]
+    }
+};
